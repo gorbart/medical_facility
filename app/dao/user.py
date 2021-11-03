c6646b4462d71b135fa326613151b7c28435ca7a
gorbart <bartek.go3@gmail.com>
2021-11-04 00:00:17 +0100

    added daos for user

---
+++ app/dao/user.py
@@ -0,0 +1,24 @@
+from config import user_collection
+from app.dao.common import add_entity, delete_entity, get_entities, get_entity, update_entity
+
+async def get_users():
+    return await get_entities(user_collection)
+
+
+async def get_user(user_id: str):
+    return await get_entity(user_collection, user_id)
+
+async def get_user_by_login(login: str):
+    return await user_collection.find_one({"login": login})
+
+
+async def add_user(user_data: dict):
+    return await add_entity(user_collection, user_data)
+
+
+async def update_user(user_id: str, user_data: dict):
+    return await update_entity(user_collection, user_data, user_id)
+
+
+async def delete_user(user_id: str):
+    return await delete_entity(user_collection, user_id)
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,12 @@
+from enum import IntEnum
 from app.models.base import Person
+
+class UserType(IntEnum):
+    ADMIN = 0
+    MANAGER = 1
+    NURSE = 2
 
 class User(Person):
     login: str
     password: str
+    user_type: UserType
